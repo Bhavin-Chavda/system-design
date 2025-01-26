ec424ebba06c91a5089aedb72f79576b24da05d7
Bhavin-Chavda <bhavinchavda096@gmail.com>
2025-01-26 22:41:19 +0530

    Builder Design Pattern Practice

---
+++ src/main/java/com/example/SystemDesign/BuilderDesignPattern/BuilderPracticeMain.java
@@ -0,0 +1,18 @@
+package com.example.SystemDesign.BuilderDesignPattern;
+
+public class BuilderPracticeMain {
+
+
+    public static void main(String[] args){
+
+        Director directorObj1 = new Director(new EngineeringStudentBuilder());
+        Director directorObj2 = new Director(new MBAStudentBuilder());
+
+        Student engineerStudent = directorObj1.createStudent();
+        Student mbaStudent =directorObj2.createStudent();
+
+        System.out.println(engineerStudent.toString());
+        System.out.println(mbaStudent.toString());
+
+    }
+}
